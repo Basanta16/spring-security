3371345db4571ad647f38fe9226b50673651f109
Basanta16 <thapabasanta16@gmail.com>
2026-07-11 00:13:03 +0100

    feat: Custom Authentication Exception implemented

--- src/main/java/com/basanta/security/config/ProjectSecurityConfigProd.java
+++ src/main/java/com/basanta/security/config/ProjectSecurityConfigProd.java
@@ -4,26 +4,25 @@ package com.basanta.security.config;
 import com.basanta.security.exception.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class ProjectSecurityConfig {
+@Profile("prod")
+public class ProjectSecurityConfigProd {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests((requests)-> requests
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests((requests)-> requests
                 .requestMatchers("/api/**").authenticated()
-                .requestMatchers("/notices", "/contacts", "/error").permitAll());
+                .requestMatchers("/notices", "/contacts", "/error", "/register").permitAll());
 //        http.authorizeHttpRequests((requests)-> requests.anyRequest().denyAll());
 //        http.authorizeHttpRequests((requests)-> requests.anyRequest().permitAll());
         http.formLogin(withDefaults());
@@ -31,11 +30,10 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    @Bean
+    /*@Bean
     public UserDetailsService userDetailsService(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
-    }
-
+    }*/
 
 //    @Bean
 //    public UserDetailsService userDetailsService() {
@@ -44,18 +42,13 @@ public class ProjectSecurityConfig {
 //        return new InMemoryUserDetailsManager(user, admin);
 //    }
 //
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 //
 //    @Bean
 //    public CompromisedPasswordChecker compromisedPasswordChecker(){
 //        return new HaveIBeenPwnedRestApiPasswordChecker();
 //    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
 }
